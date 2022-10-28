7905c10c488c7352c9fdec0a9dde80c318b20b13
aboveStars <yunuskorkmazis384@outlook.com>
2022-10-29 00:28:13 +0300

    Python-GCD-Example Liste Sortlama Başarılı

--- gcdCalculator.py
+++ gcdCalculator.py
@@ -5,9 +5,10 @@
 # 6 12 15
 def GCD_Calculator(*args):
     numbersList = list(args)
+    numbersList.sort(reverse=True)
     return numbersList
 
-print(GCD_Calculator(6,12,15))
+print(GCD_Calculator(6,15,12))
 
 
 
